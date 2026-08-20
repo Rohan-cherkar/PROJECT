a6515d47e3589d97492696d92670ce8b51c33d88
Rohan-cherkar <cherkarrohan@gmail.com>
2026-08-20 11:48:06 +0530

    styling improved

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
 app.use(methodOverride("_method"));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+app.set("view engine", "ejs"); 
+app.set("views", path.join(__dirname, "views"));
+
+app.engine("ejs", ejsMate);
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Roamly";
 main()
